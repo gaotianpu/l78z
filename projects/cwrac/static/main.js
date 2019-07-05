3679b3be8736655dc28ec102036b90846f23aa95
gaotianpu <gaotianpu@gmail.com>
2019-07-05 12:09:40 +0800

    clean codes

--- projects/cwrac/static/main.js
+++ projects/cwrac/static/main.js
@@ -1,5 +1,14 @@
 "use strict";
-//1. 工具栏、当前选中的是什么工具? 基础工具：移动、点、直线、圆！ 解锁工具
+//TODO:
+// 1. 锚定，用户绘图时，如果很接近某个交点、某条直线、某个圆周等，释放鼠标时，点落的位置应该是最接近的交点或线上。
+// 2. 超出可视区域部分的处理? 画步可移动、缩放？
+// 
+
+// 
+//1. 工具栏
+// 基础工具：移动、点、直线、圆
+// 高级工具：需逐步解锁，垂直平分线、垂线、二分角、平行线、
+// 当前选中的是什么工具? ！ 解锁工具
 //2. 不同的工具对鼠标事件响应不一样？鼠标的点击、释放
 
 //1. 有一个数组，用于记录每个步骤干嘛
@@ -26,25 +35,46 @@
         var ctx = canvas.getContext('2d');
         var canvas_rect = canvas.getBoundingClientRect();
 
-        function get_w_b(item){
+        //各种数学计算公式 
+
+        function calc_distance(x, y, x1, y1) {
+            //计算两点之间的距离
+            return Math.round(Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)));
+        }
+
+        function calc_line_parameters(item) {
+            //计算直线的斜率weight、偏置量bias、直线在画布左右边界的交点
+            //y = w*x + b 
             var w = (item.y - item.y1) / (item.x - item.x1); //直线的斜率
             var b = item.y - w * item.x;  //直线的偏置项
-            return [w,b];
+
+            //直线在画布左右的边界点
+            var left = { 'x': 0, 'y': Math.round(b) };
+            var right = { 'x': canvas_rect.width, 'y': Math.round(w * canvas_rect.width + b) };
+
+            return { 'weight': w, 'bias': b, 'left': left, 'right': right }
         }
 
-        function get_2line_intersect_point(item,item1){
-            var wb = get_w_b(item);
-            var wb1 = get_w_b(item1);
-
+        function calc_line2_intersect_point(item, item1) {
+            //计算两条直线的交点
+            // y = a0*x + b0
+            // y1 = a1*x1 + b1 , 相交点，x=x1, y=y1
             // x = (b1-b0)/(a0-a1), 将x代入任一直线方程求解
-            // y= a0*((b1-b0)/(a0-a1)) + b0
+            // y= a0*x + b0 , 将x代入公式得到y 
+            var line = calc_line_parameters(item);
+            var line1 = calc_line_parameters(item1);
+            var x = (line1.bias - line.bias) / (line.weight - line1.weight);
+            var y = line.weight * x + line.bias;
 
-            var x = (wb1[1]-wb[1])/(wb[0]-wb1[0]);
-            var y = wb[0] * x + wb[1];
-            return {'x':x,'y':y}; 
+            if (x <= 0 || y <= 0 || x > canvas_rect.width || y > canvas_rect.height) {
+                //TODO: 画布移动？
+                return false;
+            }
+            return { 'x': Math.round(x), 'y': Math.round(y) };
         }
 
-        function get_intersect_points() {
+        function get_all_intersect_points() {
+            //获取所有的相交点
             intersect_points = [];
 
             //计算两个图形相交的点
@@ -53,39 +83,56 @@
                 return;
             }
             for (var i = 0; i < len; i++) {
-                var obj0 = operation_list[i];
-                var obj0_wb = get_w_b(obj0);
-
                 for (var j = i + 1; j < len; j++) {
                     if (i == j) {
                         continue;
                     }
-                    if(operation_list[i].type == Tools.LINE && operation_list[j].type == Tools.LINE){
-                        var point = get_2line_intersect_point(operation_list[i],operation_list[j]);
-                        intersect_points.push(point); 
+
+                    if (operation_list[i].type == Tools.LINE && operation_list[j].type == Tools.LINE) {
+                        //两直线相交
+                        var point = calc_line2_intersect_point(operation_list[i], operation_list[j]);
+                        if (point) {
+                            intersect_points.push(point);
+                        }
                     }
 
-                    console.log(operation_list[i], operation_list[j]);
+                    if (operation_list[i].type == Tools.LINE && operation_list[j].type == Tools.CIRCLE) {
+                        //计算线段和圆的交点
+                        //https://thecodeway.com/blog/?p=932 
+                        // (x-x0)²+(y-y0)²=r²  //圆心坐标为(x0，y0),r半径
+                        // y = ax + b 
+                        // (x-x0)² + (ax + b-y0)²=r² 
+                        // x² + x0² - 2*x0*x + a²x² + (b-y0)² + 2*a*(b-y0)*x = r²
+                        // (a²+1)x² + 2*(a*(b-y0) - x0) * x = r² - (b-y0)²
+                    }
+
+                    if (operation_list[i].type == Tools.CIRCLE && operation_list[j].type == Tools.LINE) {
+
+
+                    }
+
+                    // console.log(operation_list[i], operation_list[j]);
                 }
 
             }
+
+            //点去重，1.相交点去重，2.与用户绘制的点去重
         }
 
-
-        function calc_distance(x, y, x1, y1) {
-            return Math.round(Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)));
-        }
 
         function render() {
             if (current_tool == Tools.HAND) {
+                //TODO ?
                 return;
             }
 
-            get_intersect_points();
+
 
             canvas_rect = canvas.getBoundingClientRect();
             ctx.clearRect(0, 0, canvas_rect.width, canvas_rect.height); //TODO
 
+            //绘制各种交点
+            get_all_intersect_points();
             for (var item of intersect_points) {
                 ctx.beginPath();
                 ctx.arc(item.x, item.y, 2, 0, Math.PI * 2, true);
@@ -106,19 +153,13 @@
 
                 //线
                 if (item.type == Tools.LINE) {
-                    //需要根据用户选定的2点，确定直线斜率，画出直线
-
-                    var w = (item.y - item.y1) / (item.x - item.x1); //直线的斜率
-                    var b = item.y - w * item.x;  //直线的偏置项
-
-                    var start = [0, Math.round(b)];
-                    var end = [canvas_rect.width, Math.round(w * canvas_rect.width + b)];
-
+                    //绘制直线
+                    var line = calc_line_parameters(item);
                     ctx.beginPath();
                     ctx.lineWidth = 0.3;
                     ctx.strokeStyle = 'gray';
-                    ctx.moveTo(start[0], start[1]);
-                    ctx.lineTo(end[0], end[1]);
+                    ctx.moveTo(line.left.x, line.left.y);
+                    ctx.lineTo(line.right.x, line.right.y);
                     ctx.stroke();
                     ctx.closePath();
 
