e14e53be620dfbe1e88726138eb9fb5924586097
gaotianpu <gaotianpu@gmail.com>
2019-09-18 15:33:46 +0800

    cc

--- cpp/note_1_HelloWord/chinese_sub.cpp
+++ cpp/note_1_HelloWord/chinese_sub.cpp
@@ -6,6 +6,8 @@
 //
 #include <iostream>
 
+int is_zh_cn(char p);  //判断字符是否为汉字
+
 int main() {
     //一个char多少个字节？ 1个
     std::cout << "sizeof(char)=" << sizeof(char) << std::endl;
@@ -14,15 +16,25 @@ int main() {
     // std::cout << "sizeof(高)=" << sizeof('高') << std::endl;
 
     //字符串
-    std::cout << "sizeof(a)=" << sizeof("a") << std::endl;    // 1+1
+    std::cout << "sizeof(a)=" << sizeof("a") << std::endl;    // 1+1 '\0'
     std::cout << "sizeof(ab)=" << sizeof("ab") << std::endl;  // 2+1
 
     //一个中文3个字节？
     std::cout << "sizeof(高)=" << sizeof("高") << std::endl;      // 3+1 ?
     std::cout << "sizeof(高天)=" << sizeof("高天") << std::endl;  // 3+3+1 ?
     std::string cn_str = "欢迎welcome来到我的世界";
+    std::cout << "cn_str length:" << cn_str.length() << std::endl;
     std::cout << cn_str.substr(0, 9) << std::endl;
     std::cout << cn_str.c_str() << std::endl;
 
     //不同的编码对汉字长度的影响？
+
+    //截取n个字符长度？
+}
+
+int is_zh_ch(char p) {
+    if (~(p >> 8) == 0) {
+        return 1;
+    }
+    return -1;
 }
