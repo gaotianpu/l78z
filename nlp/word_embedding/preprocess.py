e980885072072459535be5fa8ceb7ff3775542a4
gaotianpu <gaotianpu@gmail.com>
2020-09-14 13:22:07 +0800

    clear

--- nlp/word_embedding/preprocess.py
+++ nlp/word_embedding/preprocess.py
@@ -8,45 +8,60 @@ import pickle
 import time
 
 
-def gen_vocabulary(corp_file, vocab_file):
-    vocab_set = set()
-    with open(corp_file, 'r') as f:
-        for i, line in enumerate(f):
-            l = line.replace(" ", "").strip()
-            for char in l:
-                if char not in vocab_set:
-                    vocab_set.add(char)
-    words = sorted(vocab_set)
+class Preprocess():
+    def __init__(self, corp_file, out_root):
+        self.corp_file = corp_file
+        self.out_root = out_root
+        self.vocab_file = self.out_root + "vocab.txt"
+        self.cbow_file = self.out_root + "cbow4.txt"
 
-    words.insert(0, "UNK")
-    with open(vocab_file, 'w') as f:
-        f.write("\n".join(words))
-
-
-def generate_cbow_data(raw_text, context_size=2):
-    """一段分词好的wordlist，产出cbow需要的训练数据"""
-    data = []
-    for i in range(context_size, len(raw_text) - context_size):
-        context = []
-        for ii in range(-context_size, context_size+1):
-            if ii != 0:
-                context.append(raw_text[i+ii])
-        target = raw_text[i]
-        context.append(target)
-        data.append(context)
-    return data
-
-
-def gen_cbow_data(corp_file, cbow_file):
-    with open(cbow_file, 'w') as fw:
-        with open(corp_file, 'r') as fr:
-            for i, line in enumerate(fr):
+    def gen_vocabulary(self):
+        """生成词典"""
+        vocab_set = set()
+        with open(self.corp_file, 'r') as f:
+            for i, line in enumerate(f):
                 l = line.replace(" ", "").strip()
-                data = generate_cbow_data(l, 4)
-                if data:
-                    fw.write("\n".join(["\t".join(d) for d in data]))
-                    fw.write("\n") 
+                for char in l:
+                    if char not in vocab_set:
+                        vocab_set.add(char)
+        words = sorted(vocab_set)
+
+        words.insert(0, "UNK")
+        with open(self.vocab_file, 'w') as f:
+            f.write("\n".join(words))
+
+    def generate_cbow_data(self, raw_text, context_size=2):
+        """一段分词好的wordlist，产出cbow需要的训练数据"""
+        data = []
+        for i in range(context_size, len(raw_text) - context_size):
+            context = []
+            for ii in range(-context_size, context_size+1):
+                if ii != 0:
+                    context.append(raw_text[i+ii])
+            target = raw_text[i]
+            context.append(target)
+            data.append(context)
+        return data
+
+    def gen_cbow_data(self):
+        with open(self.cbow_file, 'w') as fw:
+            with open(self.corp_file, 'r') as fr:
+                for i, line in enumerate(fr):
+                    l = line.replace(" ", "").strip()
+                    data = self.generate_cbow_data(l, 4)
+                    if data:
+                        fw.write("\n".join(["\t".join(d) for d in data]))
+                        fw.write("\n")
+
+    def generate(self):
+        self.gen_vocabulary()
+        self.gen_cbow_data()
+
+
+def run():
+    p = Preprocess("data/zhihu.txt", "data/")
+    p.generate()
+
 
 if __name__ == "__main__":
-    gen_vocabulary("data/zhihu.txt", "data/vocab.txt")
-    gen_cbow_data("data/zhihu.txt", "data/cbow4.txt")
+    run()
